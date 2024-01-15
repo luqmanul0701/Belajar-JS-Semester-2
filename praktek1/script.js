bd51bcd5484aea7f1cd0ca52d414244391a8479e
luqmanul0701 <naufalluqmanul29@gmail.com>
2024-01-15 11:39:45 +0800

    belajar javascript

---
+++ praktek1/script.js
@@ -0,0 +1,15 @@
+const negara = "Makedonia Utara";
+const benua = "Eropa";
+let populasi = "2,065,000";
+let kepulauan = true;
+var bahasa = "Albania";
+
+console.log(negara);
+console.log(benua);
+console.log(populasi);
+console.log(kepulauan);
+console.log(bahasa);
+
+
+
+
